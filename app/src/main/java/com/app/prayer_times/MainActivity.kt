5738c6592b09450a0b0bc5a5f8c93982d8b69056
Salahuddin Saiet <Salahuddinsaiet.10@gmail.com>
2023-12-22 08:51:26 +0200

    Updated project files

--- app/src/main/java/com/app/prayer_times/MainActivity.kt
+++ app/src/main/java/com/app/prayer_times/MainActivity.kt
@@ -15,14 +15,7 @@ import com.app.prayer_times.ui.theme.PrayerTimesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PrayerTimesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
