23fa6dc655414965b39bcd69ac705f351969b116
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-13 11:41:04 +0200

    Removed exact alarm permission request

--- app/src/main/java/com/app/prayer_times/utils/permissions/Permission.kt
+++ app/src/main/java/com/app/prayer_times/utils/permissions/Permission.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
+import com.app.prayer_times.utils.debug.Logger
 
 object Permission {
     fun getNotificationPermission(activity: Activity) : Boolean {
@@ -12,20 +13,6 @@ object Permission {
                 ActivityCompat.requestPermissions(activity,
                     arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
                     1)
-                return false
-            }
-        }
-        return true
-    }
-
-    fun getExactAlarmPermission(activity: Activity): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            if (!hasPermission(android.Manifest.permission.SCHEDULE_EXACT_ALARM, activity)) {
-                ActivityCompat.requestPermissions(
-                    activity,
-                    arrayOf(android.Manifest.permission.SCHEDULE_EXACT_ALARM),
-                    2
-                )
                 return false
             }
         }
