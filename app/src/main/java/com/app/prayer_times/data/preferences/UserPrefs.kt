cfa5403b4f93a6f3926ec6287d0a8b9e59a602d3
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-14 14:26:58 +0200

    Added class for managing user preferences

--- app/src/main/java/com/app/prayer_times/data/preferences/UserPrefs.kt
+++ app/src/main/java/com/app/prayer_times/data/preferences/UserPrefs.kt
@@ -1,4 +1,37 @@
 package com.app.prayer_times.data.preferences
 
-class UserPrefs {
+import android.content.Context
+import android.content.SharedPreferences
+
+class UserPrefs(context: Context) {
+    private val sharedPrefs: SharedPreferences =
+        context.getSharedPreferences("user_settings", Context.MODE_PRIVATE)
+    private val editor = sharedPrefs.edit()
+
+    fun getString(key: String, default: String?): String? {
+        return sharedPrefs.getString(key, default)
+    }
+
+    fun getBool(key: String, default: Boolean): Boolean {
+        return sharedPrefs.getBoolean(key, default)
+    }
+
+    fun getInt(key: String, default: Int): Int {
+        return sharedPrefs.getInt(key, default)
+    }
+
+    fun setString(key: String, value: String) {
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun setBool(key: String, value: Boolean) {
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    fun setInt(key: String, value: Int) {
+        editor.putInt(key, value)
+        editor.apply()
+    }
 }
