03133af1dd1cb1d9c904098d547e7c3d2a2c9f85
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-14 19:41:21 +0200

    Now schedules an inexact alarm

--- app/src/main/java/com/app/prayer_times/utils/schedulers/MyJobScheduler.kt
+++ app/src/main/java/com/app/prayer_times/utils/schedulers/MyJobScheduler.kt
@@ -1,8 +1,37 @@
 package com.app.prayer_times.utils.schedulers
 
-import android.app.job.JobScheduler
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import com.app.prayer_times.utils.datetime.Time
+import com.app.prayer_times.utils.debug.Logger
+import com.app.prayer_times.utils.receivers.MyJobReceiver
 
 class MyJobScheduler(private val context: Context) {
-    val scheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    fun scheduleJob(): Boolean {
+        try {
+            val alarmIntent = Intent(context, MyJobReceiver::class.java).let { intent ->
+                PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_IMMUTABLE
+                )
+            }
+            alarmManager.setInexactRepeating(
+                AlarmManager.RTC,
+                Time(0, 0).toMillis(), //Midnight
+                AlarmManager.INTERVAL_DAY,
+                alarmIntent
+            )
+            return true
+
+        } catch (e: SecurityException) {
+            Logger.logErr("Failed to schedule reminder notification: $e")
+        }
+        return false
+    }
 }
