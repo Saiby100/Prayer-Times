87182ea5be1d6136978dada08f25d9ac91a83f54
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-02-15 10:54:05 +0200

    Added cancel job function

--- app/src/main/java/com/app/prayer_times/utils/schedulers/MyJobScheduler.kt
+++ app/src/main/java/com/app/prayer_times/utils/schedulers/MyJobScheduler.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import com.app.prayer_times.utils.datetime.Time
 import com.app.prayer_times.utils.debug.Logger
 import com.app.prayer_times.utils.receivers.MyJobReceiver
+import com.app.prayer_times.utils.receivers.NotificationReceiver
 
 class MyJobScheduler(private val context: Context) {
     private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
@@ -23,7 +24,7 @@ class MyJobScheduler(private val context: Context) {
             }
             alarmManager.setInexactRepeating(
                 AlarmManager.RTC,
-                Time(0, 0).toMillis(), //Midnight
+                Time(0, 10).toMillis(),
                 AlarmManager.INTERVAL_DAY,
                 alarmIntent
             )
@@ -33,5 +34,21 @@ class MyJobScheduler(private val context: Context) {
             Logger.logErr("Failed to schedule reminder notification: $e")
         }
         return false
+    }
+
+    fun cancelJob() {
+        try {
+            val alarmIntent = Intent(context, NotificationReceiver::class.java)
+
+            alarmManager.cancel(PendingIntent.getBroadcast(
+                context,
+                0,
+                alarmIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            ))
+            Logger.logDebug("Scheduling job cancel successful")
+        } catch (e: Exception) {
+            Logger.logErr("Failed to cancel schedule operation: $e")
+        }
     }
 }
